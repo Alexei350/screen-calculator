412f8deb9c7daf26b6e098507a29a30fa921d136
Alexei <alexeisecretti@gmail.com>
2020-01-08 23:32:01 -0300

    Adiciona arquivos do projeto existente

---
+++ src/ScreenCalculator/ScreenCalculator/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScreenCalculator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double cm = double.TryParse(txtPol.Text, out double valor) ? valor * 2.54 : 0;
+
+            double escala = Math.Sqrt(Math.Pow(cm, 2) / (Math.Pow(16, 2) + Math.Pow(9, 2)));
+
+            double altura = escala * 9;
+            double largura = escala * 16;
+
+            lblAltura.Text = "Altura: " + altura.ToString("N1") + "cm";
+            lblLargura.Text = "Largura: " + largura.ToString("N1") + "cm";
+        }
+    }
+}
