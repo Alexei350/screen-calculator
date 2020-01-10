66d1e280ae5182e14db38585be72fc98fd497ed8
Alexei <alexeisecretti@gmail.com>
2020-01-10 10:52:34 -0300

    Implementa input em centímetros

--- src/ScreenCalculator/ScreenCalculator/MainPage.xaml.cs
+++ src/ScreenCalculator/ScreenCalculator/MainPage.xaml.cs
@@ -22,21 +22,42 @@ namespace ScreenCalculator
 
         private void Entry_TextChanged(object sender, TextChangedEventArgs e)
         {
-            double valorDiagPol = double.TryParse(txtPol.Text, out double valor) ? valor : 0;
-            double ValorDiagCm  = valorDiagPol * 2.54;
+            AtualizaValores();
+        }
+
+        private void pkrUnidade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AtualizaValores();
+        }
+
+        private void AtualizaValores()
+        {
+            double valorDiagPol;
+            double valorDiagCm;
+
+            if (pkrUnidade.SelectedIndex == 0)
+            {
+                valorDiagPol = double.TryParse(txtInput.Text, out double valor) ? valor : 0;
+                valorDiagCm = valorDiagPol * 2.54;
+            }
+            else
+            {
+                valorDiagCm = double.TryParse(txtInput.Text, out double valor) ? valor : 0;
+                valorDiagPol = valorDiagCm / 2.54;
+            }
 
             string diagPol = valorDiagPol.ToString("N1");
-            string diagCm  = ValorDiagCm.ToString("N1");
+            string diagCm = valorDiagCm.ToString("N1");
 
-            string altPol  = (valorDiagPol * 0.49091).ToString("N1");
+            string altPol = (valorDiagPol * 0.49091).ToString("N1");
             string largPol = (valorDiagPol * 0.87247).ToString("N1");
 
-            string altCm  = (ValorDiagCm * 0.49091).ToString("N1");
-            string largCm = (ValorDiagCm * 0.87247).ToString("N1");
-            
+            string altCm = (valorDiagCm * 0.49091).ToString("N1");
+            string largCm = (valorDiagCm * 0.87247).ToString("N1");
+
             lblDiagonal.Text = $"{diagCm}cm {diagPol}\"";
-            lblAltura.Text   = $"{altCm}cm {altPol}\"";
-            lblLargura.Text  = $"{largCm}cm {largPol}\"";
+            lblAltura.Text = $"{altCm}cm {altPol}\"";
+            lblLargura.Text = $"{largCm}cm {largPol}\"";
         }
     }
 }
